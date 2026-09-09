e884e8e5c25b958a472d1972c27da0f2a03ef22b
Boyan Iliev <boyan.georgiev.iliev@gmail.com>
2026-09-09 12:00:00 +0000

    test: cover tokenize drops digits

--- test_wordfreq.py
+++ test_wordfreq.py
@@ -6,3 +6,10 @@ def test_tokenize_lowercases_and_splits():
 
 def test_tokenize_keeps_apostrophes():
     assert "don't" in wordfreq.tokenize("Don't stop")
+
+
+def test_tokenize_drops_digits():
+    assert wordfreq.tokenize("abc 123 def") == ["abc", "def"]
+
+def test_min_length_filter():
+    assert wordfreq.tokenize("a bb ccc", min_length=3) == ["ccc"]
